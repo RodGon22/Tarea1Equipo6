39105df5841737ea60ca423e732224b8ed250875
RodGon22 <110698240+RodGon22@users.noreply.github.com>
2025-09-10 17:58:26 -0600

    Valores Faltantes

--- Tarea1.py
+++ Tarea1.py
@@ -3,9 +3,14 @@
 ''' TAREA 1. CIENCIA DE DATOS. ALE, LUZ Y RODRI'''
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
 
 #Ajustamos la ruta al archivo de datos .csv:
-file_path = "/Users/aleborrego/Downloads/Tarea 1 Ciencia de Datos/data.csv"
+file_path = "/Users/rodri/Downloads/data.csv"
 
 #Creamos un DataFrame con las características en filas y las muestras en columnas.
 df = pd.read_csv(file_path)
@@ -21,5 +26,29 @@ df_data = df.drop(df.index[0:9])
 df_data = df_data.reset_index(drop=True)
 
 df_data = df_data.rename(columns={'Site Code': 'Año'})
+
+#============Valores Faltantes===============
+
+# Porcentaje de valores faltantes por columna
+porcentaje_faltantes = (df_data.isnull().sum() / len(df)) * 100
+porcentaje_faltantes = porcentaje_faltantes[porcentaje_faltantes > 0].sort_values(ascending=False)
+
+print("\nPorcentaje de valores faltantes por variable:")
+for col, percentage in porcentaje_faltantes.items():
+    print(f"{col}: {percentage:.2f}%")
+
+#Grafica ilustrativa
+plt.figure(figsize=(12, 8))
+faltante_data = df_data.isnull().sum()
+faltante_data = faltante_data[faltante_data > 0]
+faltante_data.sort_values(ascending=True).plot(kind='barh',color='red')
+plt.title('Cantidad de valores faltantes por variable')
+plt.xlabel('Número de valores faltantes')
+plt.tight_layout()
+#plt.savefig('valores_faltantes.png', dpi=300)
+plt.show()
+
+
+#===========
 
 
