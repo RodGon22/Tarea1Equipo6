7fdb259dd185b0f6d798f924b769e95780051772
Alejandra <aleborrego@MacBook-Air-de-Alejandra.local>
2025-09-10 20:44:23 -0600

    Graficas

--- Tarea1.py
+++ Tarea1.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 
+#============Exploración de los datos===============
 
 #Ajustamos la ruta al archivo de datos .csv:
-file_path = "/Users/rodri/Downloads/data.csv"
+file_path = "/Users/aleborrego/Downloads/Tarea 1 Ciencia de Datos/data.csv"
 
 #Creamos un DataFrame con las características en filas y las muestras en columnas.
 df = pd.read_csv(file_path)
@@ -25,7 +26,66 @@ df_data = df.drop(df.index[0:9])
 #Recorremos el índice para que inicie en 0
 df_data = df_data.reset_index(drop=True)
 
+#Cambiamos el nombre de la columna 'Site Code' para que sea más claro que se trata del año de la medición
 df_data = df_data.rename(columns={'Site Code': 'Año'})
+
+#Resumen breve de los datos
+df_data.describe()
+
+#Gráfica de dispersión de todos los datos (sin distinción)
+plt.figure(figsize=(10, 6))
+
+for col in df_data.columns[1:]:
+    plt.scatter(df_data['Año'], pd.to_numeric(df_data[col], errors='coerce'),
+             marker='o', color = 'pink')
+    
+plt.xlabel('Año')
+plt.ylabel('Valor')
+plt.title('Observaciones vs Año')
+plt.show()
+
+##Gráfica de dispersión de todos los datos (con distinción por sitio)
+plt.figure(figsize=(10, 6))
+
+for col in df_data.columns[1:]:
+    plt.scatter(df_data['Año'], pd.to_numeric(df_data[col], errors='coerce'),
+             marker='o')
+    
+plt.xlabel('Año')
+plt.ylabel('Valor')
+plt.title('Observaciones vs Año')
+plt.show()
+
+#Gráfica de dispersión para distinguir y analizar menos sitios
+plt.figure(figsize=(10, 6))
+
+for col in df_data.columns[1:6]:
+    plt.scatter(df_data['Año'], pd.to_numeric(df_data[col], errors='coerce'),
+             marker='o', label = col)
+
+plt.xlabel('Año')
+plt.ylabel('Valor')
+plt.title('Todas las variables vs Año')
+plt.legend()
+plt.show()
+
+#Más gráficas considerando todos los datos
+
+#Histograma
+sns.histplot(pd.to_numeric(df_data.iloc[:, 1:].values.flatten(),
+                           errors='coerce'), kde=False, color="pink")
+plt.show()
+    
+#Densidad kernel
+sns.kdeplot(pd.to_numeric(df_data.iloc[:, 1:].values.flatten(), errors='coerce'),
+                          color="pink", fill=True)
+plt.show()
+    
+#Boxplot
+sns.boxplot(pd.to_numeric(df_data.iloc[:, 1:].values.flatten(),errors='coerce'),
+                          color="pink", fill=True)
+plt.show()
+
 
 #============Valores Faltantes===============
 
@@ -48,6 +108,7 @@ plt.tight_layout()
 #plt.savefig('valores_faltantes.png', dpi=300)
 plt.show()
 
+#a
 
 #===========
 
